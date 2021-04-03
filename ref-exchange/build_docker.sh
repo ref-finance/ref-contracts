3eb1cc1dd9196000a9fb79f44308879e3e15a36e
referencedev <goi65io3903d@protonmail.com>
2021-04-03 15:31:32 +0000

    Reuse existing container for building images

--- ref-exchange/build_docker.sh
+++ ref-exchange/build_docker.sh
@@ -5,13 +5,24 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker run \
+NAME="build_ref_exchange"
+
+if docker ps -a --format '{{.Names}}' | grep -Eq "^${NAME}\$"; then
+    echo "Container exists"
+else
+docker create \
      --mount type=bind,source=$DIR/..,target=/host \
      --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
+     --name=$NAME \
      -w /host/ref-exchange \
      -e RUSTFLAGS='-C link-arg=-s' \
+     -it \
      nearprotocol/contract-builder \
-     /bin/bash -c "rustup target add wasm32-unknown-unknown; cargo build --target wasm32-unknown-unknown --release"
+     /bin/bash
+fi
+
+docker start $NAME
+docker exec -it $NAME /bin/bash -c "rustup target add wasm32-unknown-unknown; cargo build --target wasm32-unknown-unknown --release"
 
 mkdir -p res
 cp $DIR/../target/wasm32-unknown-unknown/release/ref_exchange.wasm $DIR/../res/ref_exchange_release.wasm
