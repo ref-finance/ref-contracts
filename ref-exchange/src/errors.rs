06dfdd39bd3527208ed773f47db36ae91757c5f9
mohamedalichelbi <mohamedalichelbi@icloud.com>
2021-04-07 19:59:44 +0200

    potential typo in error code

--- ref-exchange/src/errors.rs
+++ ref-exchange/src/errors.rs
@@ -7,7 +7,7 @@ pub const ERR12_TOKEN_NOT_WHITELISTED: &str = "E12: token not whitelisted";
 // Account Deposits //
 
 pub const ERR21_TOKEN_NOT_REG: &str = "E21: token not registered";
-pub const ERR22_NOT_ENOUGH_TOKENS: &str = "E23: not enough tokens in deposit";
+pub const ERR22_NOT_ENOUGH_TOKENS: &str = "E22: not enough tokens in deposit";
 // pub const ERR23_NOT_ENOUGH_NEAR: &str = "E23: not enough NEAR in deposit";
 pub const ERR24_NON_ZERO_TOKEN_BALANCE: &str = "E24: non-zero token balance";
 pub const ERR25_CALLBACK_POST_WITHDRAW_INVALID: &str =
