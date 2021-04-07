3765f58af06d24493247110a0e282a711655f241
Mike Purvis <mikedotexe@gmail.com>
2021-04-06 23:25:21 -0700

    add callback to withdraw function, add sim test ensuring deposits remain after failure

--- ref-exchange/src/account_deposit.rs
+++ ref-exchange/src/account_deposit.rs
@@ -7,7 +7,7 @@ use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::json_types::{ValidAccountId, U128};
 use near_sdk::{assert_one_yocto, env, near_bindgen, AccountId, Balance};
 
-use crate::utils::{ext_fungible_token, GAS_FOR_FT_TRANSFER};
+use crate::utils::{ext_fungible_token, ext_self, GAS_FOR_FT_TRANSFER};
 use crate::*;
 
 const MAX_ACCOUNT_LENGTH: u128 = 64;
@@ -89,7 +89,7 @@ impl Contract {
     /// Fails if not enough balance on this account to cover storage.
     pub fn register_tokens(&mut self, token_ids: Vec<ValidAccountId>) {
         let sender_id = env::predecessor_account_id();
-        let mut deposits = self.get_account_depoists(&sender_id);
+        let mut deposits = self.get_account_deposits(&sender_id);
         for token_id in token_ids {
             deposits.register(token_id.as_ref());
         }
@@ -100,7 +100,7 @@ impl Contract {
     /// Panics if the balance of any given token is non 0.
     pub fn unregister_tokens(&mut self, token_ids: Vec<ValidAccountId>) {
         let sender_id = env::predecessor_account_id();
-        let mut deposits = self.get_account_depoists(&sender_id);
+        let mut deposits = self.get_account_deposits(&sender_id);
         for token_id in token_ids {
             deposits.unregister(token_id.as_ref());
         }
@@ -113,23 +113,43 @@ impl Contract {
     #[payable]
     pub fn withdraw(&mut self, token_id: ValidAccountId, amount: U128, unregister: Option<bool>) {
         assert_one_yocto();
-        let token_id: AccountId = token_id.into();
+        let token_id_acct: AccountId = token_id.clone().into();
         let amount: u128 = amount.into();
         let sender_id = env::predecessor_account_id();
-        let mut deposits = self.get_account_depoists(&sender_id);
-        deposits.sub(&token_id, amount);
+        let mut deposits = self.get_account_deposits(&sender_id);
+        // Note: subtraction and deregistration will be reverted if the promise fails.
+        deposits.sub(&token_id_acct, amount);
         if unregister == Some(true) {
-            deposits.unregister(&token_id);
+            deposits.unregister(&token_id.as_ref());
         }
         self.deposited_amounts.insert(&sender_id, &deposits);
         ext_fungible_token::ft_transfer(
-            sender_id.try_into().unwrap(),
+            sender_id.clone().try_into().unwrap(),
             amount.into(),
             None,
-            &token_id,
+            &token_id_acct,
             1,
             GAS_FOR_FT_TRANSFER,
-        );
+        ).then(ext_self::exchange_callback_post_withdraw(
+            token_id,
+            sender_id.clone().try_into().unwrap(),
+            amount.into(),
+            unregister,
+            &env::current_account_id(),
+            0,
+            GAS_FOR_FT_TRANSFER,
+        ));
+    }
+
+    #[private]
+    pub fn exchange_callback_post_withdraw(&mut self, token_id: ValidAccountId, sender_id: ValidAccountId, amount: U128, unregister: Option<bool>) {
+        let mut deposits = self.get_account_deposits(&sender_id.as_ref());
+        // This reverts the changes from withdraw function
+        deposits.add(&token_id.as_ref(), amount.0);
+        if unregister == Some(true) {
+            deposits.register(&token_id.as_ref());
+        }
+        self.deposited_amounts.insert(&sender_id.as_ref(), &deposits);
     }
 }
 
