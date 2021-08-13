28bf2b24cc0cfddf4941c762d8571a8828ebab01
marco_mac <sun.dsk1@icloud.com>
2021-08-13 23:03:08 +0800

    postpone audit_01

--- ref-exchange/src/account_deposit.rs
+++ ref-exchange/src/account_deposit.rs
@@ -43,14 +43,14 @@ impl Account {
 
     /// Returns amount of $NEAR necessary to cover storage used by this data structure.
     pub fn storage_usage(&self) -> Balance {
-        // [AUDIT_01]
-        (MIN_ACCOUNT_DEPOSIT_LENGTH + self.tokens.len() as u128 * (4 + MAX_ACCOUNT_LENGTH + 16))
+        // [AUDIT_01_postpone]
+        (MIN_ACCOUNT_DEPOSIT_LENGTH + self.tokens.len() as u128 * (MAX_ACCOUNT_LENGTH + 16))
             * env::storage_byte_cost()
     }
 
     /// Returns how much NEAR is available for storage.
     pub fn storage_available(&self) -> Balance {
-        // [AUDIT_01]
+        // [AUDIT_01] avoid math overflow
         let locked = self.storage_usage();
         if self.near_amount > locked {
             self.near_amount - locked
