0f311c7853ff1de3962603846f60b525745705e9
marco_mac <sun.dsk1@icloud.com>
2021-08-13 23:15:10 +0800

    postpone audit_01

--- ref-exchange/src/account_deposit.rs
+++ ref-exchange/src/account_deposit.rs
@@ -45,8 +45,8 @@ impl Account {
 
     /// Returns amount of $NEAR necessary to cover storage used by this data structure.
     pub fn storage_usage(&self) -> Balance {
-        // [AUDIT_01] the first 4 bytes are string length when BorshSerialize haspmap key.
-        (MIN_ACCOUNT_DEPOSIT_LENGTH + self.tokens.len() as u128 * (4 + MAX_ACCOUNT_LENGTH + 16))
+        // [AUDIT_01_postpone]
+        (MIN_ACCOUNT_DEPOSIT_LENGTH + self.tokens.len() as u128 * (MAX_ACCOUNT_LENGTH + 16))
             * env::storage_byte_cost()
     }
 
