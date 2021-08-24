e813114d36e7c4a46fbb65b52ee299f4751fb368
marco_mac <sun.dsk1@icloud.com>
2021-08-24 16:33:55 +0800

    update

--- ref-farming/tests/test_single_farm.rs
+++ ref-farming/tests/test_single_farm.rs
@@ -14,22 +14,27 @@ mod common;
 #[test]
 fn single_farm_startat_0() {
     let root = init_simulator(None);
+
+    println!("----->> Prepare accounts.");
     let owner = root.create_user("owner".to_string(), to_yocto("100"));
     let farmer1 = root.create_user("farmer1".to_string(), to_yocto("100"));
     let farmer2 = root.create_user("farmer2".to_string(), to_yocto("100"));
-    println!("----->> owner and 2 farmers prepaired.");
+    println!("<<----- owner and 2 farmers prepared.");
 
+    println!("----->> Prepare ref-exchange and swap pool.");
     let (pool, token1, token2) = prepair_pool_and_liquidity(
         &root, &owner, farming_id(), vec![&farmer1, &farmer2]);
-    println!("----->> The pool prepaired.");
-
+    println!("<<----- The pool prepaired.");
 
     // deploy farming contract and register user
+    println!("----->> Deploy farming and register farmers.");
     let farming = deploy_farming(&root, farming_id(), owner.account_id());
     call!(farmer1, farming.storage_deposit(None, None), deposit = to_yocto("1")).assert_success();
     call!(farmer2, farming.storage_deposit(None, None), deposit = to_yocto("1")).assert_success();
-    
+    println!("<<----- farming deployed, farmers registered.");
+
     // create farm
+    println!("----->> Create farm.");
     let farm_id = "swap@0#0".to_string();
     let out_come = call!(
         owner,
@@ -44,8 +49,13 @@ fn single_farm_startat_0() {
     );
     out_come.assert_success();
     assert_eq!(Value::String(farm_id.clone()), out_come.unwrap_json_value());
+    println!("<<----- Farm {} created at #{}, ts:{}.", 
+    farm_id,
+    root.borrow_runtime().current_block().block_height, 
+    root.borrow_runtime().current_block().block_timestamp);
 
     // deposit reward
+    println!("----->> Deposit reward to turn farm Running.");
     call!(
         root,
         token1.storage_deposit(Some(to_va(farming_id())), None),
@@ -58,19 +68,69 @@ fn single_farm_startat_0() {
         deposit = 1
     )
     .assert_success();
+    show_farminfo(&farming, farm_id.clone(), true);
+    println!("<<----- Farm {} deposit reward at #{}, ts:{}.", 
+    farm_id,
+    root.borrow_runtime().current_block().block_height, 
+    root.borrow_runtime().current_block().block_timestamp);
 
-    // staking lpt
+    // farmer1 staking lpt 
+    println!("----->> Farmer1 staking lpt.");
     let out_come = call!(
         farmer1,
         pool.mft_transfer_call(":0".to_string(), to_va(farming_id()), to_yocto("1").into(), None, "".to_string()),
         deposit = 1
     );
     out_come.assert_success();
-    println!("----->> Farmer1 staked liquidity at #{}, ts:{}.", 
+    println!("<<----- Farmer1 staked liquidity at #{}, ts:{}.", 
     root.borrow_runtime().current_block().block_height, 
     root.borrow_runtime().current_block().block_timestamp);
     let farm_info = show_farminfo(&farming, farm_id.clone(), false);
     assert_farming(&farm_info, "Running".to_string(), to_yocto("10"), 0, 0, 0, 0, 0);
-    
+    let user_seeds = show_userseeds(&farming, farmer1.account_id(), false);
+    assert_eq!(user_seeds.get(&String::from("swap@0")).unwrap().0, to_yocto("1"));
+    let unclaim = show_unclaim(&farming, farmer1.account_id(), farm_id.clone(), false);
+    assert_eq!(unclaim.0, 0_u128);
 
+    println!("----->> move to 60 secs later.");
+    assert!(root.borrow_runtime_mut().produce_blocks(60).is_ok());
+    println!("<<----- Chain goes 60 blocks, now #{}, ts:{}.", 
+    root.borrow_runtime().current_block().block_height, 
+    root.borrow_runtime().current_block().block_timestamp);
+    let farm_info = show_farminfo(&farming, farm_id.clone(), false);
+    assert_farming(&farm_info, "Running".to_string(), to_yocto("10"), 1, 0, 0, to_yocto("1"), 0);
+    let user_seeds = show_userseeds(&farming, farmer1.account_id(), false);
+    assert_eq!(user_seeds.get(&String::from("swap@0")).unwrap().0, to_yocto("1"));
+    let unclaim = show_unclaim(&farming, farmer1.account_id(), farm_id.clone(), false);
+    assert_eq!(unclaim.0, to_yocto("1"));
+
+    // farmer2 staking lpt 
+    println!("----->> Farmer2 staking lpt.");
+    let out_come = call!(
+        farmer2,
+        pool.mft_transfer_call(":0".to_string(), to_va(farming_id()), to_yocto("1").into(), None, "".to_string()),
+        deposit = 1
+    );
+    out_come.assert_success();
+    println!("<<----- Farmer2 staked liquidity at #{}, ts:{}.", 
+    root.borrow_runtime().current_block().block_height, 
+    root.borrow_runtime().current_block().block_timestamp);
+    let farm_info = show_farminfo(&farming, farm_id.clone(), false);
+    assert_farming(&farm_info, "Running".to_string(), to_yocto("10"), 1, 1, 0, to_yocto("1"), 0);
+    let user_seeds = show_userseeds(&farming, farmer2.account_id(), false);
+    assert_eq!(user_seeds.get(&String::from("swap@0")).unwrap().0, to_yocto("1"));
+    let unclaim = show_unclaim(&farming, farmer2.account_id(), farm_id.clone(), false);
+    assert_eq!(unclaim.0, 0_u128);
+
+    println!("----->> move to 60 secs later.");
+    assert!(root.borrow_runtime_mut().produce_blocks(60).is_ok());
+    println!("<<----- Chain goes 60 blocks, now #{}, ts:{}.", 
+    root.borrow_runtime().current_block().block_height, 
+    root.borrow_runtime().current_block().block_timestamp);
+    let farm_info = show_farminfo(&farming, farm_id.clone(), false);
+    assert_farming(&farm_info, "Running".to_string(), to_yocto("10"), 2, 1, 0, to_yocto("2"), 0);
+    let unclaim = show_unclaim(&farming, farmer1.account_id(), farm_id.clone(), false);
+    assert_eq!(unclaim.0, to_yocto("1.5"));
+    let unclaim = show_unclaim(&farming, farmer2.account_id(), farm_id.clone(), false);
+    assert_eq!(unclaim.0, to_yocto("0.5"));
 }
