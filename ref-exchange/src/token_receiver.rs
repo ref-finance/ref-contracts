262852281c3c03eddbb824a8a2739c40ed7987a9
marco_mac <sun.dsk1@icloud.com>
2021-09-03 15:45:38 +0800

    enable feature instant-swap

--- ref-exchange/src/token_receiver.rs
+++ ref-exchange/src/token_receiver.rs
@@ -75,7 +75,6 @@ impl Contract {
 }
 
 #[near_bindgen]
-#[allow(unreachable_code)]
 impl FungibleTokenReceiver for Contract {
     /// Callback on receiving tokens by this contract.
     /// `msg` format is either "" for deposit or `TokenReceiverMessage`.
@@ -91,9 +90,7 @@ impl FungibleTokenReceiver for Contract {
             self.internal_deposit(sender_id.as_ref(), &token_in, amount.into());
             PromiseOrValue::Value(U128(0))
         } else {
-            // [AUDIT14] shutdown instant swap from interface
-            env::panic(b"Instant Swap Feature Not Open Yet");
-
+            // instant swap
             let message =
                 serde_json::from_str::<TokenReceiverMessage>(&msg).expect("ERR_MSG_WRONG_FORMAT");
             match message {
