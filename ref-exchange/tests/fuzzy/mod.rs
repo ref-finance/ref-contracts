858cea8784cc93166a10b020bd8b10605b6e7b94
Marco <sun.dsk1@gmail.com>
2021-11-16 09:14:26 +0800

    merge fuzzy-test

---
+++ ref-exchange/tests/fuzzy/mod.rs
@@ -0,0 +1,8 @@
+pub mod create_simple_pool;
+pub mod direct_swap;
+pub mod pool_swap;
+pub mod liquidity_manage;
+pub mod types;
+pub mod constants;
+pub mod utils;
+
---
+++ ref-exchange/tests/fuzzy/types.rs
@@ -0,0 +1,52 @@
+use near_sdk::serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use near_sdk::AccountId;
+use near_sdk_sim::{
+    ContractAccount, UserAccount,
+};
+use near_sdk::json_types::U128;
+use test_token::ContractContract as TestToken;
+
+#[derive(Default)]
+pub struct OperationContext {
+    pub token_contract_account: HashMap<AccountId, ContractAccount<TestToken>>
+}
+
+#[derive(Debug)]
+pub enum Preference {
+    CreateSamplePool, 
+    DirectSwap,
+    PoolSwap,
+    AddLiquidity
+}
+
+#[derive(Debug)]
+pub enum Scenario {
+    Normal,
+    Token1NotRegistered, 
+    Token2NotRegistered, 
+    Token1NoAccount,
+    Token2NoAccount,
+    NoStorageDeposit,
+}
+
+#[derive(Debug)]
+pub enum DSScenario {
+    Normal,
+    LiquidityEmpty,
+    TokenInZero,
+    TokenOutZero,
+}
+
+#[derive(Debug)]
+pub struct Operator {
+    pub user: UserAccount,
+    pub preference: Preference
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
+pub struct RefStorageState {
+    pub deposit: U128,
+    pub usage: U128,
+}
