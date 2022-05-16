4123ff8688ed3a4577ad6fd28435117538b89933
Marco <47070476+marco-sundsk@users.noreply.github.com>
2022-05-16 11:59:51 +0800

    Fix bug on modify_seed_min_deposit (#82)
    * fix bug on modify_seed_min_deposit
    * make release of ref-farming

--- ref-farming/tests/fuzzy/utils.rs
+++ ref-farming/tests/fuzzy/utils.rs
@@ -18,8 +18,8 @@ use crate::fuzzy::{
 
 near_sdk_sim::lazy_static_include::lazy_static_include_bytes! {
     TEST_TOKEN_WASM_BYTES => "../res/test_token.wasm",
-    EXCHANGE_WASM_BYTES => "../res/ref_exchange_release.wasm",
-    FARM_WASM_BYTES => "../res/ref_farming_release.wasm",
+    EXCHANGE_WASM_BYTES => "../res/ref_exchange.wasm",
+    FARM_WASM_BYTES => "../res/ref_farming.wasm",
 }
 
 pub fn deploy_farming(root: &UserAccount, farming_id: AccountId, owner_id: AccountId) -> ContractAccount<Farming> {
