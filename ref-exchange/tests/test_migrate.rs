1fddf59d357224525fdc63b8807b8ee48591d6b8
Marco <sun.dsk1@gmail.com>
2022-05-29 22:27:43 +0800

    release exchange v1.5.1 and farming v1.0.4

--- ref-exchange/tests/test_migrate.rs
+++ ref-exchange/tests/test_migrate.rs
@@ -46,7 +46,7 @@ fn test_upgrade() {
     .assert_success();
     let metadata = get_metadata(&pool);
     // println!("{:#?}", metadata);
-    assert_eq!(metadata.version, "1.5.0".to_string());
+    assert_eq!(metadata.version, "1.5.1".to_string());
     assert_eq!(metadata.exchange_fee, 4);
     assert_eq!(metadata.referral_fee, 1);
     assert_eq!(metadata.state, RunningState::Running);
